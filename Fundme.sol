33a91b6d2908d4b05a765dbb6775d8e938a40c2c
XiaoZhong233 <610596547@qq.com>
2024-12-28 13:27:20 +0800

    记录收款

--- Fundme.sol
+++ Fundme.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.8.20;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-
+//收款
+//记录投资人并查看
+//在锁定期内，达到目标值，生产商可以提款
+//在锁定期内，没有达到目标值，投资人在锁定期以后退款
 contract Fundme{
     AggregatorV3Interface internal dataFeed;
 
     mapping (address => uint256) internal fundersToAmount;
 
     uint256 MINIMUN_VALUE = 1 * 10 ** 18;
+
+    uint constant TARGET = 10 * 10 ** 18;
+
+    address public  owner;
 
     /**
      * Network: Sepolia
@@ -20,6 +27,7 @@ contract Fundme{
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
+        owner = msg.sender;
     }
 
     function getChainlinkDataFeedLatestAnswer() public view returns (int) {
@@ -41,5 +49,42 @@ contract Fundme{
 
     function convertEthToUSD(uint256 amountCount) internal view returns (uint){
         return amountCount * uint(getChainlinkDataFeedLatestAnswer()) / 10**8;
+    }
+
+    //提取资产
+    function getFund() external {
+        require(convertEthToUSD(address(this).balance) >= TARGET, "Target is not reached");
+        require(msg.sender == owner, "this function can only be called by owner");
+        //transfer: 如果交易失败，则交易会回滚，但是gas不会
+        // payable(msg.sender).transfer(address(this).balance);
+        //send: 会有返回值 return true false
+        // bool success = payable(msg.sender).send(address(this).balance);
+        // require(success, "tx failed");
+        //call evm推荐方法 可以获取到函数的返回值和交易结果
+        bool success;
+        (success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "tx failed");
+
+    }
+
+    function refund() external {
+        require(convertEthToUSD(address(this).balance) < TARGET, "Target is reached");
+        uint256 amount = fundersToAmount[msg.sender];
+        require(amount>0, "there is no fund for you");
+        //最好是先清空余额 再转账
+        fundersToAmount[msg.sender] = 0;
+        bool success;
+        (success, ) = payable(msg.sender).call{value: amount}("");
+        if(!success){
+            fundersToAmount[msg.sender] = amount;
+        }
+        require(success, "tx failed");
+    }
+
+
+    //合约所有权转移
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == owner, "this function can only be called by owner");
+        owner = newOwner;
     }
 }
